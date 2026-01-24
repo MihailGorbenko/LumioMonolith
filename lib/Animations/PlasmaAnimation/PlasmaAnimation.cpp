ce68da9e99101e12035cc8a2ddd1e40cfe0ee594
MihailGorbenko <gomihagle@gmail.com>
2026-01-24 23:38:53 +0200

    finished PlasmaAnimation

--- lib/Animations/PlasmaAnimation/PlasmaAnimation.cpp
+++ lib/Animations/PlasmaAnimation/PlasmaAnimation.cpp
@@ -5,11 +5,6 @@ PlasmaAnimation::PlasmaAnimation(LedMatrix& m)
 	: AnimationBase(m, PLASMA_DEFAULT_HUE, PLASMA_DEFAULT_SAT, PLASMA_DEFAULT_VAL) {
 }
 
-void PlasmaAnimation::setColorHSV(uint8_t h, uint8_t s, uint8_t v) {
-	// Plasma: hue drifts dynamically, but base hue is stored and used as offset
-	AnimationBase::setColorHSV(h, s, v);
-}
-
 void PlasmaAnimation::render() {
 	if (!matrix) return;
 	matrix->clear();
@@ -19,16 +14,18 @@ void PlasmaAnimation::render() {
 	if (w <= 0) w = 1;
 	if (hgt <= 0) hgt = 1;
 
-	uint16_t t = (uint16_t)(millis() / 4);
+	uint32_t t32 = millis() / 4U;
+	uint8_t t = (uint8_t)(t32 & 0xFF);
+	uint8_t t1 = (uint8_t)((t32 >> 1) & 0xFF);
 
 	for (int x = 0; x < w; ++x) {
-		uint8_t sx = sin8((uint8_t)(x * 8 + (t & 0xFF)));
+		uint8_t sx = sin8((uint8_t)(x * 8 + t));
 		for (int y = 0; y < hgt; ++y) {
-			uint8_t sy = sin8((uint8_t)(y * 48 + ((t >> 1) & 0xFF)));
+			uint8_t sy = sin8((uint8_t)(y * 48 + t1));
 			uint8_t p = (uint8_t)((sx + sy) >> 1);
 
 			// Hue slowly drifts, value is modulated by plasma field
-			uint8_t hOut = (uint8_t)(hue + (t >> 3) + (p >> 2));
+			uint8_t hOut = (uint8_t)(hue + (t32 >> 3) + (p >> 2));
 			// allow full brightness at peaks; keep small floor of 16
 			uint8_t vOut = scale8(val, qadd8(16, p));
 			matrix->setPixelHSV(x, y, hOut, sat, vOut);
